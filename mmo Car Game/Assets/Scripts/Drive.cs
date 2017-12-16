a5ea68ffd5e65a0491a50621ebc746a9b0c5d1aa
Moran Abecassis <moranmoa@gmail.com>
2017-12-16 12:20:00 +0200

    first

---
+++ mmo Car Game/Assets/Scripts/Drive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+	float Speed = 10.0F;
+	float rotationSpeed = 100.0F;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translation = CrossPlatformInputManager.GetAxis("Vertical") * Speed;
+		float rotation = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
+
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+	}
+}
